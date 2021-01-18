2eac99d9d0930799ba4f3051555d79f3288698ba
chenruishan <370875342@qq.com>
2021-01-18 11:05:47 +0800

    it could run，still need debug

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 //https://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-likely-collisions-in-2d-space--gamedev-374
 //origin in java,modify it to c++11;
 #include <iostream>
-
+#include <vector>
+using namespace std;
 class Rectangle
 {
 
@@ -13,6 +14,13 @@ public:	 float getX() { return X;};
 		 float getY() { return Y; };
 		 float getWidth() { return width; };
 		 float getHeight() { return height; };
+public:	  Rectangle()
+{
+	X = 0.;
+	Y = 0.;
+	width = 0.;
+	height = 0.;
+}
 public:	  Rectangle(float x,float y,float w,float h)
 {
 	X = x;
@@ -28,25 +36,41 @@ private: int MAX_OBJECTS = 10;
 		 int MAX_LEVELS = 5;
 		 
 private:   int level;
-		   List objects;
+		   vector<Rectangle> objects;
 		   Rectangle bounds;
-		   Quadtree[] nodes;
+		   Quadtree* nodes[4];
+
+public:	   Quadtree()
+{
+	level = 0;
+	//vector<Rectangle> _objects;
+	//_objects.reserve(10);
+	//objects = _objects;
+	bounds = Rectangle(0.,0.,0.,0.);
+	//for (int i = 0; i < 4; i++) {
+	//	nodes[i] = 0;
+	//}
+}
 public:	   Quadtree(int pLevel, Rectangle pBounds)
 {
 	level = pLevel;
-	objects = new std:vector<Rectangle>;
+	//vector<Rectangle> _objects;
+	//_objects.reserve(10);
+	//objects = _objects;
 	bounds = pBounds;
-	nodes = new Quadtree[4];
+	//for (int i = 0; i < 4; i++) {
+	//	nodes[i] = new Quadtree();
+	//}
 }
 
 public:	void clear()
 {
 	objects.clear();
 
-	for (int i = 0; i < nodes.length; i++) {
-		if (nodes[i] != null) {
-			nodes[i].clear();
-			nodes[i] = null;
+	for (int i = 0; i < 4; i++) {
+		if (nodes[i] != 0) {
+			nodes[i]->clear();
+			nodes[i] = 0;
 		}
 	}
 }
@@ -56,10 +80,10 @@ private:  void split() {
 	int x = (int)bounds.getX();
 	int y = (int)bounds.getY();
 
-	nodes[0] = new Quadtree(level + 1, new Rectangle(x + subWidth, y, subWidth, subHeight));
-	nodes[1] = new Quadtree(level + 1, new Rectangle(x, y, subWidth, subHeight));
-	nodes[2] = new Quadtree(level + 1, new Rectangle(x, y + subHeight, subWidth, subHeight));
-	nodes[3] = new Quadtree(level + 1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
+	nodes[0] = new Quadtree(level + 1, Rectangle(x + subWidth, y, subWidth, subHeight));
+	nodes[1] = new Quadtree(level + 1, Rectangle(x, y, subWidth, subHeight));
+	nodes[2] = new Quadtree(level + 1, Rectangle(x, y + subHeight, subWidth, subHeight));
+	nodes[3] = new Quadtree(level + 1, Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
 }
 
 /*
@@ -103,65 +127,80 @@ private: int getIndex(Rectangle pRect) {
  * objects to their corresponding nodes.
  */
 public: void insert(Rectangle pRect) {
-			 if (nodes[0] != null) {
+			 if (nodes[0] != 0) {
 				 int index = getIndex(pRect);
 
 				 if (index != -1) {
-					 nodes[index].insert(pRect);
+					 nodes[index]->insert(pRect);
 
 					 return;
 				 }
 			 }
 
-			 objects.add(pRect);
+			 objects.push_back(pRect);
 
 			 if (objects.size() > MAX_OBJECTS && level < MAX_LEVELS) {
-				 if (nodes[0] == null) {
+				 if (nodes[0] == 0) {
 					 split();
 				 }
 
+				 
 				 int i = 0;
-				 while (i < objects.size()) {
-					 int index = getIndex(objects.get(i));
-					 if (index != -1) {
-						 nodes[index].insert(objects.remove(i));
+				 while (i< objects.size()) {
+					 int index = getIndex(objects[i]);
+					 
+					 if (index != -1) {						 
+						 nodes[index]->insert(objects[i]);								 
+						 objects.erase(std::begin(objects) + i);
 					 }
 					 else {
-						 i++;
+						 i++;						 
 					 }
 				 }
 			 }
 		 }
 
-		/*
+/*
  * Return all objects that could collide with the given object
  */
-		public List retrieve(List returnObjects, Rectangle pRect) {
+public: vector<Rectangle> retrieve(vector<Rectangle>& returnObjects, Rectangle pRect) {
 			int index = getIndex(pRect);
-			if (index != -1 && nodes[0] != null) {
-				nodes[index].retrieve(returnObjects, pRect);
+			if (index != -1 && nodes[0] != 0) {
+				nodes[index]->retrieve(returnObjects, pRect);
 			}
-
-			returnObjects.addAll(objects);
+			for (size_t i=0;i<objects.size();i++)
+			{
+				returnObjects.push_back(objects[i]);
+			}
+			
 
 			return returnObjects;
 		}
 };
-void main()
+int main()
 {
-	Quadtree quad = new Quadtree(0, new Rectangle(0, 0, 600, 600));
+	Rectangle bound = Rectangle(300., 300., 100., 100.);
+	Quadtree quad = Quadtree(0, bound);
 	quad.clear();
-	for (int i = 0; i < allObjects.size(); i++) {
-		quad.insert(allObjects.get(i));
+	vector<Rectangle> allObjects;
+	for (int j = 10; j < 100; j = j + 20)
+		for (int jj = 10; jj < 100; jj = jj + 20)
+	{
+			Rectangle b = Rectangle(j, jj, 2, 2);
+			allObjects.push_back(b);
 	}
-	List returnObjects = new ArrayList();
+	
+	for (int i = 0; i < allObjects.size(); i++) {
+		quad.insert(allObjects[i]);
+	}
+	vector<Rectangle> returnObjects;
 	for (int i = 0; i < allObjects.size(); i++) {
 		returnObjects.clear();
-		quad.retrieve(returnObjects, objects.get(i));
+		quad.retrieve(returnObjects, allObjects[i]);
 
 		for (int x = 0; x < returnObjects.size(); x++) {
 			// Run collision detection algorithm between objects
 		}
 	}
-	return;
+	return 0;
 }
